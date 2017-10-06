a8229d72ed68a5315eae3af49e3484143722b670
cinedicted <32210962+cinedicted@users.noreply.github.com>
2017-10-06 06:51:25 +0530

    Update careers.php

--- careers.php
+++ careers.php
@@ -1,4 +1,42 @@
 <?php /* Template Name: Careers */ ?>
 <?php get_header(); ?>
+<div class="container">
+    <div class="work-with-us-container col-xs-12">
+        <h1 class="work-with-us-header"> Wanna work with us ??? </h1>
+        <div class="work-with-us-image">
+            <div class="career-image"></div>
+            <div class="career-body">
+                <div>You're passionate about Cinema, creative and believe your views need to be heard outside of your head.
+                    We're reimagining Cinema and its impact. Could this be love ?</div>
 
+                <div>Shoot your article(s) to
+                    <a href = "mailto: wewantyou@cinedicted.com">wewantyou@cinedicted.com</a></div>
+
+                <div class="list-container">Think yourself hired if you CAN:
+                <ul>
+                    <li>Bleed Passion in your articles.</li>
+                 <li>Deploy Humor/Satire in abundance.</li>
+                 <li>Your English can swoon our pretty brains off its feet.</li>
+                 <li>Support your crazy thoughts with facts/valid reasoning too.</li>
+                 <li>Be empathetic & straight as an arrow too.</li>
+                 <li>You believe Perfect is Good.</li>
+                    </ul>
+                </div>
+
+                <div>What’s in it for me you ask?</div>
+                <div class="list-container">
+                    Some reasons which might compel you to lift your lazy fingers and type away:
+                    <ul><li>Receive full credit for your work along with a link back to your website/blog/portfolio.</li>
+                    <li>We’ll share your article on our social channels: Facebook, Twitter, Quora too!</li>
+                    <li>Increase your own credibility by quickly connecting with our audience.</li>
+                    <li>Rave, Rant and Review with complete creative freedom about Movies with the slightest hope that it will make a difference someday.</li>
+                    <li>Apart from this, you will be able to build a reputation for yourself as a creative writer.</li>
+                        <li>Work Wherever, Whenever</li></ul>
+                </div>
+
+                <div class="disclaimer"><b>Disclaimer:</b> The views and opinions expressed on this site are of the contributors and not necessarily those of Cinedicted. Articles published on Cinedicted may not be reproduced (in part/whole) elsewhere, without prior permission from Cinedicted</div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
