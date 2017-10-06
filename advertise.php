74d0ded95d13cb6db528572634a4feb68acb390a
cinedicted <32210962+cinedicted@users.noreply.github.com>
2017-10-06 06:48:59 +0530

    Update advertise.php

--- advertise.php
+++ advertise.php
@@ -1,1 +1,13 @@
 <?php /* Template Name: Advertise */ ?>
+<?php get_header(); ?>
+<div class="container">
+    <div class="advertise-container">
+        <h1 class="advertise-header">Advertise With Cinedicted</h1>
+
+        <div>Cinedicted is an Entertainment portal providing a Birds-eye view on Indian Cinema with focus on new narratives, behind the scenes, podcasts, reviews, 
+            interviews, memes, walk down memory lanes, Movie Industry analysis etc.</div>
+            
+        <div>For any advertising/partnership queries, please send us a mail to - <a href="mailto:advertising@cinedicted.com">advertising@cinedicted.com</a> with the subject ADVERTISING QUERY.</div>
+    </div>
+</div>
+<?php get_footer(); ?>
