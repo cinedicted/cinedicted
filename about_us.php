7de024ceed1d513755f443bd13555ab824496125
cinedicted <32210962+cinedicted@users.noreply.github.com>
2017-10-06 06:50:42 +0530

    Create about_us.php

---
+++ about_us.php
@@ -0,0 +1,29 @@
+<?php /* Template Name: AboutUs */ ?>
+<?php get_header(); ?>
+    <div class="container">
+        <div class="about-us-container">
+            <h1 class="about-us-header">About Us</h1>
+            <div class="about-us-body">
+                <div class="about-us-quote">
+                    <q>Cinema is the most beautiful fraud in the World.</q>
+                    <span>- Jean-Luc Godard</span>
+                </div>
+                
+                <div class="about-us-images">
+                    <div class="doctor"></div>
+                    <div class="marketing"></div>
+                    <div class="software"></div>
+                </div>
+                <div class="about-us-content">
+                    <div>What started as a useless conversation between a Marketing Dude, a Software Engineer and a Doctor about a forgettable movie turned into this heated discussion and an epiphany! We realized that we needed a platform to rant and rave about Cinema 
+                        as an unadulterated art-form without the bells and whistles reflecting the society we live in. Lo and Behold, Cinedicted was born to realize this idea of creating content about Cinema and its universe that our audience will become addicted to.</div>
+
+                    <div>All we can say about our writers are that they are a bunch of crazy and passionate Movie Geeks who live, breathe and eat Cinema. They speak the same language we do and hopefully you, our crazy audience will too. We gave them a pen ( Sorry, a keyboard!) and thus began their journey at Cinedicted.</div>
+
+                    <div>Our Vision is to be the No 1 Entertainment portal providing a Birds-eye view on Indian Cinema with focus on new narratives, behind the scenes, podcasts, reviews, 
+                        interviews, memes, walk down memory lanes, Movie Industry analysis etc.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
