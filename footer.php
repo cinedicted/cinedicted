b251944f8dc0588f6ea15eada7119c2dfe11f5ca
cinedicted <32210962+cinedicted@users.noreply.github.com>
2017-10-19 06:29:35 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,22 @@
-    
-        <footer>
-            my footer
+           <div class="fixed-newsletter"><?php echo do_shortcode('[wysija_form id="3"]'); ?></div>
+           <footer>
+            <div class="footer-content">
+                <ul class="footer-list">
+                    <li class="footer-list__item">
+                        <a href="/wordpress/about-us">About Us</a>
+                    </li>
+                    <li class="footer-list__item">
+                        <a href="/wordpress/contact-us">Contact Us</a>
+                    </li>
+                    <li class="footer-list__item">
+                        <a href="/wordpress/advertise-with-us">Advertise With Us</a>
+                    </li>
+                    <li class="footer-list__item">
+                        <a href="/wordpress/careers">Write For Us</a>
+                    </li>
+                    <li class="footer-list__item">Privacy Policy</li>
+                </ul>
+            </div>
         </footer>
         <?php wp_footer() ?>
     </body>
