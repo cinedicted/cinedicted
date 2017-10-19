225687290123eb5de5d8c57ee8804af7584f3ab3
cinedicted <32210962+cinedicted@users.noreply.github.com>
2017-10-19 06:32:58 +0530

    Update cinedicted.js

--- js/cinedicted.js
+++ js/cinedicted.js
@@ -1,21 +1,30 @@
 /*Menu Animation*/
-function myFunction(x) {
+function toggleMenu(x) {
     x.classList.toggle("change");
 }
 
 jQuery(document).ready(function(){
+    
+    jQuery('.hamburger').on('click', function() {
+        jQuery(this).toggleClass('change');
+        jQuery('.menu-items').slideToggle("fast");
+    });
+    
     jQuery('.reviews-list').slick({
         autoplay: false
     });
     
-    jQuery('.memes-list').slick({
-        autoplay: true
-    });
+    jQuery('.poll-list').slick({
+        autoPlay:false
+    })
     
-    jQuery('.polls-list').slick({
-        autoplay: false
+    jQuery('.memes-list').slick({
+        autoplay: false,
+        dots:true
     });
-
+    jQuery('.trailers-story').click(function () {
+        jQuery('#myModal').modal('show');
+    });
     jQuery('.nav-icon').on('click', function() {
         jQuery(this).toggleClass('change');
         jQuery('.primary-header-nav').toggleClass('show-transition')
@@ -27,5 +36,28 @@ jQuery(document).ready(function(){
         jQuery('.movie-list').removeClass('current');
         jQuery(this).addClass('current');
         jQuery("."+ tab_id + "-movie-list").addClass('current');
+    });
+    
+    jQuery('.search-icon').on('click', function() {
+        jQuery(".search-box").slideToggle("fast");
+    });
+    
+    jQuery('.close-search').on('click', function() {
+        jQuery(".search-box").slideToggle("fast");
+    });
+    
+    jQuery('.rating').each(function() {
+        var ratingVal = jQuery(this).text();
+        var setWidth = ((parseFloat(ratingVal)/5) * 100);
+        jQuery(this).siblings('.rating-filled').css('width', setWidth);
+    })
+    
+    jQuery(document).scroll(function (e) {
+        if (jQuery(window).scrollTop() > 600) {
+           jQuery('.fixed-newsletter').show();
+        }
+        else{
+            jQuery('.fixed-newsletter').hide();
+        }
     });
 });
